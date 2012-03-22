65be5614e50e90c2f4e50688809bbc7874f26b61
tomymanson@gmail.com <tomymanson@gmail.com@0760e501-44b5-5be5-50f5-b54a77fe15f6>
2012-03-22 18:12:41 +0000

    nettoyage

--- src/samples/featureExtractor/FeatureExtractor.java
+++ src/samples/featureExtractor/FeatureExtractor.java
@@ -10,6 +10,7 @@ import Filter.NoveltyMask;
 import Filter.Sauvola;
 
 import com.googlecode.javacv.cpp.opencv_core.CvArr;
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
 import com.googlecode.javacv.processing.Utils;
 
 import controlP5.Button;
@@ -73,7 +74,7 @@ public class FeatureExtractor extends PApplet {
 		imgsWidth = img[0].width;
 		imgsHeight = img[0].height;
 		double ratio = 1.0*imgsWidth/imgsHeight;
-		imgShowWidth = ((screenWidth/2-100<imgsWidth)?(((screenWidth)/2)-100):(imgsWidth/2)-100);
+		imgShowWidth = 500;//((screenWidth/2-100<imgsWidth)?(((screenWidth)/2)-100):(imgsWidth/2)-100);
 		imgShowHeight = (int) (imgShowWidth/ratio);
 
 		// lancement position du boutton de lancement de calcul
@@ -122,21 +123,21 @@ public class FeatureExtractor extends PApplet {
 			CvSize size = new CvSize();
 			size.width(imgG.width);
 			size.height(imgG.height);
+
+			IplImage ipl11 = cvCreateImage(size, IPL_DEPTH_8U, 3);
 			IplImage ipl1 = cvCreateImage(size, IPL_DEPTH_8U, 3);
-			Utils.PImageToIplImage(imgG, ipl1, true);
+			Utils.PImageToIplImage(imgG, ipl11, true);
+			cvScale(ipl11, ipl1, 0.25, 0);
 			IplImage ipl2 = cvCreateImage(size, IPL_DEPTH_8U, 3);
-			Utils.PImageToIplImage(imgD, ipl2, true);
+			IplImage ipl22 = cvCreateImage(size, IPL_DEPTH_8U, 3);
+			Utils.PImageToIplImage(imgD, ipl22, true);
+			cvScale(ipl22, ipl2, 0.25, 0);
 			IplImage ipl3 = cvCreateImage(size, IPL_DEPTH_8U, 1);
 			IplImage ipl4 = cvCreateImage(size, IPL_DEPTH_8U, 1);
 			IplImage ipl5 = cvCreateImage(size, IPL_DEPTH_8U, 1);
 			IplImage ipl6 = cvCreateImage(size, IPL_DEPTH_8U, 1);
 			IplImage iplres = cvCreateImage(size, IPL_DEPTH_8U, 1);
-			/*Sauvola sauvola = new Sauvola();
-			sauvola.filter(ipl1, ipl3);
-			sauvola.filter(ipl2, ipl1);
-			
-			cvSub(ipl1, ipl3, ipl2, null);*/
-			
+
 			// WB imG
 			IplImage r1 = cvCreateImage(ipl1.cvSize(), ipl1.depth(), 1);
 			IplImage g1 = cvCreateImage(ipl1.cvSize(), ipl1.depth(), 1);
@@ -147,7 +148,7 @@ public class FeatureExtractor extends PApplet {
 			b1=etirerHistogramme(b1);
 			cvMerge(r1, g1, b1, null, ipl1);
 			cvCvtColor(ipl1, ipl3, CV_RGB2GRAY);
-			
+
 			// WB imD
 			IplImage r2 = cvCreateImage(ipl1.cvSize(), ipl1.depth(), 1);
 			IplImage g2 = cvCreateImage(ipl1.cvSize(), ipl1.depth(), 1);
@@ -158,14 +159,18 @@ public class FeatureExtractor extends PApplet {
 			b2=etirerHistogramme(b2);
 			cvMerge(r2, g2, b2, null, ipl2);
 			cvCvtColor(ipl2, ipl4, CV_RGB2GRAY);
-			
-			
+
+
 			Sauvola sauvola = new Sauvola();
 			sauvola.filter(ipl3, ipl5);
 			sauvola.filter(ipl4, ipl6);
-		cvSmooth(ipl5, ipl5, CV_MEDIAN, 7);
+
+			IplConvKernel B = cvCreateStructuringElementEx(11,11, 5, 5, CV_SHAPE_ELLIPSE,null );
+			IplImage temp = cvCreateImage(ipl5.cvSize(), ipl5.depth(), 2);
+			cvMorphologyEx( ipl5, ipl5, temp, B, CV_MOP_ERODE,3);
+			cvMorphologyEx( ipl5, ipl5, temp, B, CV_MOP_DILATE,2);
 			cvAbsDiff(ipl6, ipl5, iplres);
-			
+			cvSub(ipl5, ipl6, iplres, null);
 			PImage res = Utils.toPImage(iplres);
 
 			imgResult = res;
@@ -175,47 +180,47 @@ public class FeatureExtractor extends PApplet {
 		}
 
 	}
-	
+
 	IplImage etirerHistogramme(IplImage img){
-		   int x, y, i;
-		   int[] plusSombre = new int[3];
-		   int[] plusClair = new int[3];
-		   CvScalar temp;
-		   IplImage res=cvCreateImage(cvSize(img.width(), img.height()), IPL_DEPTH_8U, 1);
-		 
-		   /* Initialisation */
-		   for(i=0; i<3; i++){
-		      plusSombre[i]=256;
-		      plusClair[i]=256;
-		   }
-		 
-		   /* Recherche des extremums */
-		   for(x=0; x<img.width(); x++){
-		      for(y=0; y<img.height(); y++){
-		         temp=cvGet2D(img, y, x);
-		            if(temp.val(0)<plusSombre[0] || plusSombre[0]>255)
-		               plusSombre[0]=(int) temp.val(0);
-		           
-		            if(temp.val(0)>plusClair[0] || plusClair[0]>255)
-		               plusClair[0]=(int) temp.val(0);
-		         
-		      }
-		   }
-		 
-		   /* Traitement */
-		   for(x=0; x<img.width(); x++){
-		      for(y=0; y<img.height(); y++){
-		         temp=cvGet2D(img, y, x);
-		       
-		            temp.val(0,(temp.val(0)-plusSombre[0])*255/(plusClair[0]-plusSombre[0]));
-		         
-		 
-		         cvSet2D(res, y, x, temp);
-		      }
-		   }
-		 
-		   return res;
+		int x, y, i;
+		int[] plusSombre = new int[3];
+		int[] plusClair = new int[3];
+		CvScalar temp;
+		IplImage res=cvCreateImage(cvSize(img.width(), img.height()), IPL_DEPTH_8U, 1);
+
+		/* Initialisation */
+		for(i=0; i<3; i++){
+			plusSombre[i]=256;
+			plusClair[i]=256;
 		}
+
+		/* Recherche des extremums */
+		for(x=0; x<img.width(); x++){
+			for(y=0; y<img.height(); y++){
+				temp=cvGet2D(img, y, x);
+				if(temp.val(0)<plusSombre[0] || plusSombre[0]>255)
+					plusSombre[0]=(int) temp.val(0);
+
+				if(temp.val(0)>plusClair[0] || plusClair[0]>255)
+					plusClair[0]=(int) temp.val(0);
+
+			}
+		}
+
+		/* Traitement */
+		for(x=0; x<img.width(); x++){
+			for(y=0; y<img.height(); y++){
+				temp=cvGet2D(img, y, x);
+
+				temp.val(0,(temp.val(0)-plusSombre[0])*255/(plusClair[0]-plusSombre[0]));
+
+
+				cvSet2D(res, y, x, temp);
+			}
+		}
+
+		return res;
+	}
 
 	boolean play = true;
 	public void keyPressed() {
